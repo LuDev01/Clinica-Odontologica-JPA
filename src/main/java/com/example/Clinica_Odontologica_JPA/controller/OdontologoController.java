fc29c44ebce407fc2c0505a768c7a19f7fd102d3
Luisa Toro <luisatoro0121@hotmail.com>
2024-06-25 20:06:01 -0500

    Implementción de Swagger

--- src/main/java/com/example/Clinica_Odontologica_JPA/controller/OdontologoController.java
+++ src/main/java/com/example/Clinica_Odontologica_JPA/controller/OdontologoController.java
@@ -3,6 +3,10 @@ package com.example.Clinica_Odontologica_JPA.controller;
 import com.example.Clinica_Odontologica_JPA.entity.Odontologo;
 import com.example.Clinica_Odontologica_JPA.exception.ResourceNotFoundException;
 import com.example.Clinica_Odontologica_JPA.service.OdontologoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,12 +16,13 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/odontologos")
-
+@Tag(name = "Controller de Odontologos", description = "permite registrar, eliminar y listar")
 public class OdontologoController {
     @Autowired
     private OdontologoService odontologoService;
 
     @GetMapping
+    @Parameter(description = "nos permite listar todos los odontologos")
     public ResponseEntity<List<Odontologo>> listarTodos() {
         return ResponseEntity.ok(odontologoService.listarTodos());
     }
@@ -32,6 +37,8 @@ public class OdontologoController {
     }
 
     @PostMapping
+    @Operation(summary = "nos permite registrar", description = "enviar odontologo sin id")
+    @ApiResponse(responseCode = "200", description = "odontologo creado con exito")
     public ResponseEntity<Odontologo>crearOdontologo(@RequestBody Odontologo odontologo){
         return ResponseEntity.ok(odontologoService.crearOdontologo(odontologo));
     }
